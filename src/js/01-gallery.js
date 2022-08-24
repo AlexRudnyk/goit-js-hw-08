62dd8e88b59c22846b090d484dac7c1bd8784a0f
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-08-24 18:12:48 +0300

    Готовое задание 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+console.log('galleryEl', galleryEl);
+
+// console.log(createGalleryItemsMarkup(galleryItems));
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+    })
+    .join('');
+}
+
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('afterbegin', itemsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
