74b7f78c21520b6c23eddc3f2721bab85faf9fc6
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-08-24 20:56:10 +0300

    Готовое задание 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const player = new Player('vimeo-player', {
+  id: 'vimeo - player',
+  width: 640,
+});
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+const throtteled = throttle(onPlay, 1_000);
+
+player.on('timeupdate', throtteled);
+
+let timePosition = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(timePosition)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
